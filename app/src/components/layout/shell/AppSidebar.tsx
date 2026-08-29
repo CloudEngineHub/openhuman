8c70f86bd2da0aadecab6dbae04e19f6b2e5e304
Steven Enamakel <enamakel@tinyhumans.ai>
2026-08-29 19:33:58 +0300

    chore: files changed app/src/components/layout/shell/AppSidebar.tsx
    Auto-committed-on: macbook
    Co-authored-by: Medulla <medulla@tinyhumans.ai>

--- app/src/components/layout/shell/AppSidebar.tsx
+++ app/src/components/layout/shell/AppSidebar.tsx
@@ -3,10 +3,7 @@ import { useEffect } from 'react';
 import { LuPanelLeftOpen } from 'react-icons/lu';
 
 import { useT } from '../../../lib/i18n/I18nContext';
-import { APP_VERSION } from '../../../utils/config';
-import ConnectionIndicator from '../../ConnectionIndicator';
 import {
-  SidebarFooter,
   SidebarContent as SidebarScrollRegion,
   SidebarSeparator,
   SidebarTrigger,
